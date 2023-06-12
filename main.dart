134dc14fde9eef74b8bb09ac89ae6f8fc22d8906
Remon Chandra Howlader <69386831+Howlader69@users.noreply.github.com>
2023-06-12 21:51:05 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,88 +1,207 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(myapp(
-    appName:'chiken app',
-  ));
+  runApp(myapp());
 }
-class myapp extends StatelessWidget{
-  final String appName;
+class myapp extends StatelessWidget {
+  const myapp({Key? key}) : super(key: key);
 
-  const myapp({ required this.appName});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'intro widget',
       home: homescreen(),
     );
   }
-
 }
-class homescreen extends StatelessWidget{
+class homescreen extends StatelessWidget {
+  const homescreen({Key? key}) : super(key: key);
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(' App headline'),
+        title: Text('Photo Gallery',style: TextStyle(
+          fontSize: 22,
+          color: Colors.white,
+        ),),
+        centerTitle: true,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green,
-              ),
-              child: Text(
-                'Navigation Drawer',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: Text('Welcome to My Photo Gallery!',style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),),
                 ),
               ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Handle item 1 press
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Handle item 2 press
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Center(
-        child: Text(
-          'hello world',
-          style: TextStyle(fontSize: 24,
-              fontWeight: FontWeight.w700,
-            color: Colors.purple
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: 'Search',
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black,width: 2),                  )
+                  ),
+                ),
+              ),
 
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 8.0,
+                children: [
+
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+
+                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
+                      onPressed: () {
+                        print('Clicked on photo');
+                      },
+                      child: Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      ),
+                  ),
+
+
+                ]
+              ),
+              SizedBox(height: 10,),
+
+              ListTile(
+                onTap: () {
+                  print('tapped');
+                },
+                onLongPress: () {
+                  print('On long press');
+                },
+                title: Text('Sample Photo 1'),
+                subtitle: Text('Category 1'),
+                leading: Icon(Icons.image, size: 32,),
+
+                tileColor: Colors.grey.withOpacity(0.3),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
+              ),
+              SizedBox(height: 10,),
+              ListTile(
+                onTap: () {
+                  print('tapped');
+                },
+                onLongPress: () {
+                  print('On long press');
+                },
+                title: Text('Sample Photo 1'),
+                subtitle: Text('Category 1'),
+                leading: Icon(Icons.image, size: 32,),
+
+                tileColor: Colors.grey.withOpacity(0.3),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
+              ),
+              SizedBox(height: 10,),
+              ListTile(
+                onTap: () {
+                  print('tapped');
+                },
+                onLongPress: () {
+                  print('On long press');
+                },
+                title: Text('Sample Photo 1'),
+                subtitle: Text('Category 1'),
+                leading: Icon(Icons.image, size: 32,),
+
+                tileColor: Colors.grey.withOpacity(0.3),
+                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
+              ),
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
-
 }
+
