2a118342495e2de89a0a5dddb0232a406fbeb5db
Remon Chandra Howlader <69386831+Howlader69@users.noreply.github.com>
2023-06-12 23:22:59 +0600

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(myapp());
+void main() {
+  runApp(MyApp());
 }
-class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,194 +12,42 @@ class myapp extends StatelessWidget {
     );
   }
 }
+
 class homescreen extends StatelessWidget {
-  const homescreen({Key? key}) : super(key: key);
-
-
+   List<String> items = [
+    'Apples',
+    'Bananas',
+    'Bread',
+    'Milk',
+    'Eggs',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Photo Gallery',style: TextStyle(
-          fontSize: 22,
-          color: Colors.white,
-        ),),
+        title: Text('My Shopping List'),
         centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  child: Text('Welcome to My Photo Gallery!',style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold
-                  ),),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                      hintText: 'Search',
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black,width: 2),                  )
-                  ),
-                ),
-              ),
-
-              Wrap(
-                spacing: 8.0,
-                runSpacing: 8.0,
-                children: [
-
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-                  ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-
-                          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 12)),
-                      onPressed: () {
-                        print('Clicked on photo');
-                      },
-                      child: Image.network(
-                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.cover,
-                      ),
-                  ),
-
-
-                ]
-              ),
-              SizedBox(height: 10,),
-
-              ListTile(
-                onTap: () {
-                  print('tapped');
-                },
-                onLongPress: () {
-                  print('On long press');
-                },
-                title: Text('Sample Photo 1'),
-                subtitle: Text('Category 1'),
-                leading: Icon(Icons.image, size: 32,),
-
-                tileColor: Colors.grey.withOpacity(0.3),
-                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
-              ),
-              SizedBox(height: 10,),
-              ListTile(
-                onTap: () {
-                  print('tapped');
-                },
-                onLongPress: () {
-                  print('On long press');
-                },
-                title: Text('Sample Photo 1'),
-                subtitle: Text('Category 1'),
-                leading: Icon(Icons.image, size: 32,),
-
-                tileColor: Colors.grey.withOpacity(0.3),
-                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
-              ),
-              SizedBox(height: 10,),
-              ListTile(
-                onTap: () {
-                  print('tapped');
-                },
-                onLongPress: () {
-                  print('On long press');
-                },
-                title: Text('Sample Photo 1'),
-                subtitle: Text('Category 1'),
-                leading: Icon(Icons.image, size: 32,),
-
-                tileColor: Colors.grey.withOpacity(0.3),
-                contentPadding: EdgeInsets.symmetric(horizontal: 16,),
-              ),
-
-
-            ],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Cart is empty')),
+              );
+            },
           ),
-        ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context,index) {
+          return ListTile(
+            leading: Icon(Icons.shopping_basket),
+            title: Text(items[index]),
+          );
+        },
       ),
     );
   }
 }
-
